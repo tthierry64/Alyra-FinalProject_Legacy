80bdb70266a5bbf21a4bd2533650da8ac5075810
tthierr64 <tomas-64@hotmail.fr>
2023-12-09 23:05:06 +0100

    Update config Vercel

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -109,7 +109,7 @@ export const abiSafetyModule = [
 ]
 export const contractSafetyModuleAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
-export const abiWETH =[
+export const abiWETH = [
   {
     "inputs": [
       {
